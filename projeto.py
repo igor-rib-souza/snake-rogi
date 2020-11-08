a3c26ffa3390d05b11839cae17d831c087866369
Igor Souza <71533908+rogi-azous@users.noreply.github.com>
2020-11-07 23:21:33 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -4,18 +4,16 @@ import random
 pygame.init()
 
 class snake:
-    start_x = random.randrange(50.850)
+    start_x = random.randrange(50,850)
     start_y = random.randrange(50,550)
     vel = 5
     comprimento = 17
     largura = 7
-    cor = (255,0,0)
-
+    cor = (255,0,0) #RGB
+    
 class comida:
     x = random.randrange(20,880)
     y = random.randrange(20,580)
     largura = 5
     comprimento = 5
-    cor = (0,0,255)
-
-
+    cor = (0,0,255) #RGB
