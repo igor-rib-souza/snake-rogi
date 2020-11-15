6a729d5f19ff021aab3543fe4cc7e2b2959d27be
Igor Souza <71533908+igor-rib-souza@users.noreply.github.com>
2020-11-15 13:58:08 -0300

    Update setup.py

--- snake-game/setup.py
+++ snake-game/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Snake-rogi.rib", # Replace with your own username
-    version="1.2.dev1",
+    name="snake-rogi.rib", # Replace with your own username
+    version="1.2",
     author="Igor Ribeiro de Souza",
     author_email="igor.r.souza@hotmail.com",
     description="The famous game snake, built with python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/igor-rib-souza/Snake",
+    url="https://github.com/igor-rib-souza/snake",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
